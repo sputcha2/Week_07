d54bee4c47110318486d9c64f70165426ec2d5cf
sputcha2 <sputcha2@illinois.edu>
2018-10-17 19:41:31 -0500

    Updated Code

--- step_01.js
+++ step_01.js
@@ -1,4 +1,3 @@
-
 //  Week 06 Express: Basic Routing
 //  Step 01
 
@@ -16,7 +15,15 @@ const port = 3000
 //  app.get(<path>, handler_function)
 //  handler function takes req (the request obj) and res (the response obj) as
 //  parameters, the function uses res to generate a response
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/:id', function(req, res) {
+  if (req.params.id == 6) {
+    res.send('About')
+  } else if (req.params.id == 2) {
+    res.send('Hello World')
+  } else {
+    res.send("Different")
+  }
+})
 
 //  Start Express Web Server i.e. start listing on the port you defined
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
